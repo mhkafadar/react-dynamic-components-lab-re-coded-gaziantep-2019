deb4394b30203176fc91e65ae49c7a697539e4aa
Mehmet Hüseyin Kafadar <mehmet@re-coded.com>
2019-07-25 15:17:48 +0000

    Automatically backed up by Learn

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -5,7 +5,7 @@ export default class ColorBox extends Component {
   render() {
     const newValue = this.props.opacity - 0.1;
 
-    if (newValue >= 0.2) {
+    if (this.props.opacity >= 0.2) {
       return (
         <div className="color-box" style={{opacity: this.props.opacity}}>
           <ColorBox opacity={newValue}/>
